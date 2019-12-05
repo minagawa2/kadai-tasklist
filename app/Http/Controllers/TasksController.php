daae5be19eb4781d362114ae9aabe5a87521e3ac
“minagawa” <carpenters147@hotmail.com>
2019-12-05 23:04:55 +0000

    modified

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -119,6 +119,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::find($id);
+        $task->delete();
+
+        return redirect('/');
     }
 }
